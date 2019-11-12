9c31caee1f90ac9e0329b10d9c9ff29afc3cb5a3
OlegParfenov <55343121+OlegParfenov@users.noreply.github.com>
2019-11-12 12:16:03 +0300

    v5

---
+++ TinyOne/gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync');
+
+gulp.task('browser-sync', async function() { // Создаем таск browser-sync
+    browserSync({ // Выполняем browser Sync
+        server: { // Определяем параметры сервера
+            baseDir: 'app' // Директория для сервера - app
+        },
+        notify: false // Отключаем уведомления
+    });
+});
+  
+gulp.task('sass', async function(){
+  gulp.src('app/scss/**/*.scss')
+    .pipe(sass()) // Using gulp-sass
+    .pipe(gulp.dest('app/css/'))
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('code', function() {
+    return gulp.src('app/*.html')
+    .pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('watch', function(){
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'));
+    gulp.watch('app/*.html', gulp.parallel('code')); 
+    // Other watchers
+  })
+
+gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch'));
