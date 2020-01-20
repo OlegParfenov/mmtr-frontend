324a27352756b8b43439234b37d23243ca483bf6
OlegParfenov <55343121+OlegParfenov@users.noreply.github.com>
2020-01-20 15:27:54 +0300

    v8.4 Настроен webpack. Исправлен баг с очисткой полей в окне создания поля.

--- TinyOne/webpack.config.js
+++ TinyOne/webpack.config.js
@@ -1,13 +1,11 @@
 const path = require('path');
-//Для сборки css
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+//Для сборки css
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 //Для сборки js
 module.exports = {
   entry: {
     main: ['./src/js/index.js']
-    // main: ['./src/js/index.js']
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -22,31 +20,31 @@ module.exports = {
           loader: "babel-loader"
         }
       },
-      {  //Для SASS
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
       }
     ]
   },
-  //Файл, в который собираются все стили
+
   plugins: [
-    new ExtractTextPlugin({ filename: 'style.css' }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
     new HtmlWebpackPlugin({
       inject: false,
       hash: true,
-      template: './src/index.html',
+      template: './public/index.html',
       filename: 'index.html'
     })
   ],
-  watch: true,
   //Для Live-reload
   devServer: {
+    contentBase: __dirname + '/',
     host: 'localhost',
     port: 8080,
     hot: true,
   },
+  // watch: true,
 };
 
