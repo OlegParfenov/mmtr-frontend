6655b393257b40069c30c593a19b915449e74865
OlegParfenov <55343121+OlegParfenov@users.noreply.github.com>
2020-02-12 14:30:18 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import {render} from 'react-dom'
 import './scss/helloworld.scss';
+
+let hello = 'Hello World';
+
+
 function HelloWord(){
     return(
         <div>
-            <h1>He Word</h1>
+            <h1>{hello}</h1>
         </div>
     )
 }
 render(<HelloWord/>, document.getElementById('root'))
+
