f1c348507622ff26cba41081bc3f15415d28b1d0
Margarita-Poliansckaya <55192837+Margarita-Poliansckaya@users.noreply.github.com>
2020-02-21 16:06:56 +0300

    Улучшена структура стилей

--- src/components/ButtonPanel.tsx
+++ src/components/ButtonPanel.tsx
@@ -4,77 +4,77 @@ import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import {PostInterface} from '../interfaces/post.interface'
 import '../scss/ButtonPanel.scss'
 
-function ButtonPanel(props: { post: PostInterface, dislikedIds: number[],dislikedIdsChangeState: any }): JSX.Element {
+function ButtonPanel(props: { post: PostInterface, dislikedIds?: number[],dislikedIdsChangeState?: any }): JSX.Element {
     let {post, dislikedIdsChangeState} = props;
 
-    function likeButtonChangeState(likeButton: any): void {
-        likeButton = likeButton.target;
-        const isPressed = likeButton.classList.contains('button-toolbar__like-button_state_pressed');
-        isPressed ? setUnPressedState(likeButton) : setPressedState(likeButton);
-    }
+    // function likeButtonChangeState(likeButton: any): void {
+    //     likeButton = likeButton.target;
+    //     const isPressed = likeButton.classList.contains('button-toolbar__like-button_state_pressed');
+    //     isPressed ? setUnPressedState(likeButton) : setPressedState(likeButton);
+    // }
 
-    function dislikeButtonChangeState(): void {
-        if (localStorage.getItem('dislikedIds') !== null){
-        let dislikedIds = JSON.parse(localStorage.dislikedIds);
-        let result = dislikedIds.filter(item => item === post.id);
-        if (result.length !== 1) {
-            dislikedIds.push(post.id);
-            localStorage.dislikedIds = JSON.stringify(dislikedIds);
-            deleteFavorites(post.id);
-            dislikedIdsChangeState(dislikedIds);
-        }
-    }
-    }
+    // function dislikeButtonChangeState(): void {
+    //     if (localStorage.getItem('dislikedIds') !== null){
+    //     let dislikedIds = JSON.parse(localStorage.dislikedIds);
+    //     let result = dislikedIds.filter(item => item === post.id);
+    //     if (result.length !== 1) {
+    //         dislikedIds.push(post.id);
+    //         localStorage.dislikedIds = JSON.stringify(dislikedIds);
+    //         deleteFavorites(post.id);
+    //         dislikedIdsChangeState(dislikedIds);
+    //     }
+    // }
+    // }
 
-    function setUnPressedState(button: any): void {
-        button.classList.remove('button-toolbar__like-button_state_pressed');
-        deleteFavorites(post.id);
-    }
+    // function setUnPressedState(button: any): void {
+    //     button.classList.remove('button-toolbar__like-button_state_pressed');
+    //     deleteFavorites(post.id);
+    // }
 
-    function setPressedState(button: any): void {
-        button.classList.add('button-toolbar__like-button_state_pressed');
-        setFavorites(post.id);
-    }
+    // function setPressedState(button: any): void {
+    //     button.classList.add('button-toolbar__like-button_state_pressed');
+    //     setFavorites(post.id);
+    // }
 
-    const likeButtonClass = isLiked(post.id) ?
-        'button-toolbar__like-button button-toolbar__like-button_state_pressed' : 'button-toolbar__like-button';
+    const likeButtonClass = 'button-toolbar__like-button';
     return (
         <ButtonToolbar className='button-toolbar'>
             <span className={likeButtonClass}
-                  onClick={(e) => likeButtonChangeState(e)}>♥</span>
+                //   onClick={(e) => likeButtonChangeState(e)}
+                  >♥</span>
             <Button variant='outline-primary' className='button-toolbar__not-like-button' size='sm'
-                    onClick={() => dislikeButtonChangeState()}>Не нравится</Button>
+                    // onClick={() => dislikeButtonChangeState()}
+                    >Не нравится</Button>
         </ButtonToolbar>
     )
 }
 
-function setFavorites(id: number): void {
-    if (localStorage.getItem('favoritesIds') !== null){
-    let favoritesIds = JSON.parse(localStorage.favoritesIds);
-    let result = favoritesIds.filter(item => item === id);
-    if (result.length !== 1) {
-        favoritesIds.push(id);
-        localStorage.favoritesIds = JSON.stringify(favoritesIds);
-    }
-}
-}
+// function setFavorites(id: number): void {
+//     if (localStorage.getItem('favoritesIds') !== null){
+//     let favoritesIds = JSON.parse(localStorage.favoritesIds);
+//     let result = favoritesIds.filter(item => item === id);
+//     if (result.length !== 1) {
+//         favoritesIds.push(id);
+//         localStorage.favoritesIds = JSON.stringify(favoritesIds);
+//     }
+// }
+// }
 
-function deleteFavorites(id: number): void {
-    if (localStorage.getItem('favoritesIds') !== null){
-    let favoritesIds = JSON.parse(localStorage.favoritesIds);
-    let index = favoritesIds.findIndex(item => item === id);
-    favoritesIds.splice(index, 1)
-    //Здесь должна быть проверка
-    localStorage.favoritesIds = JSON.stringify(favoritesIds);
-    }
-}
+// function deleteFavorites(id: number): void {
+//     if (localStorage.getItem('favoritesIds') !== null){
+//     let favoritesIds = JSON.parse(localStorage.favoritesIds);
+//     let index = favoritesIds.findIndex(item => item === id);
+//     favoritesIds.splice(index, 1)
+//     //Здесь должна быть проверка
+//     localStorage.favoritesIds = JSON.stringify(favoritesIds);
+//     }
+// }
 
-function isLiked(id: number): boolean {
-    if (localStorage.getItem('favoritesIds') !== null){
-    let favoritesIds = JSON.parse(localStorage.favoritesIds);
-    favoritesIds = favoritesIds.filter(item => item === id);
-    return favoritesIds.length > 0 ? true : false
-    }
-}
+// function isLiked(id: number): boolean {
+//     if (localStorage.getItem('favoritesIds') !== null){
+//     let favoritesIds = JSON.parse(localStorage.favoritesIds);
+//     favoritesIds = favoritesIds.filter(item => item === id);
+//     return favoritesIds.length > 0 ? true : false
+//     }
 
 export default ButtonPanel
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -7,31 +7,26 @@ import {PostInterface} from '../interfaces/post.interface';
 function Post(props: { post: PostInterface, dislikedIds: number[], dislikedIdsChangeState: any }): JSX.Element {
     let {post, dislikedIds, dislikedIdsChangeState} = props;
 
-    if (!isDisliked(post.id)) {
+    // if (!isDisliked(post.id)) 
         return (
             <div>
                 <Media className='content'>
                     <Media.Body className = 'content__news'>
                         <h5>{post.title}</h5>
                         <p>{post.body}</p>
-                        <ButtonPanel post={post}
-                                     dislikedIds={dislikedIds}
-                                     dislikedIdsChangeState={dislikedIdsChangeState}/>
+                        <ButtonPanel post={post}/>
                     </Media.Body>
                 </Media>
             </div>
         )
-    } else {
-        return <div></div>
-    }
-}
+    } 
 
-function isDisliked(id): boolean {
-    if (localStorage.getItem('dislikedIds') !== null){
-    let dislikedIds = JSON.parse(localStorage.dislikedIds);
-    dislikedIds = dislikedIds.filter(item => item === id);
-    return dislikedIds.length > 0 
-    }
-}
+// function isDisliked(id): boolean {
+//     if (localStorage.getItem('dislikedIds') !== null){
+//     let dislikedIds = JSON.parse(localStorage.dislikedIds);
+//     dislikedIds = dislikedIds.filter(item => item === id);
+//     return dislikedIds.length > 0 
+//     }
+// }
 
 export default Post
